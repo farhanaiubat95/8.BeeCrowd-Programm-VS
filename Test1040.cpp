17cb1d9cf898dca5a9d4f979981a40e20a7e4ab5
Farhana Bente Islam <21303195@iubat.edu>
2022-12-29 19:15:05 +0600

    done

--- Test1040.cpp
+++ Test1040.cpp
@@ -1,7 +1,32 @@
 #include<bits/stdc++.h>
+#include <iomanip>
 using namespace std;
 int main()
 {
-
-
+ float N1,N2,N3,N4,avgr,scr,rcal;
+ cin>>N1>>N2>>N3>>N4;
+avgr=(N1*2+N2*3+N3*4+N4)/10.0;
+cout<<fixed;
+cout<<"Media: "<< setprecision(1)<<avgr<<endl;
+if(avgr>=7.0)
+{   
+    cout<<"Aluno aprovado."<<endl;
+}else if(avgr<5.0)
+{  
+    cout<<"Aluno reprovado."<<endl;
+}else if(avgr>=5.0 && avgr<=6.9)
+{
+    cout<<"Aluno em exame."<<endl;
+    cin>>scr;
+    cout<<"Nota do exame: "<< setprecision(1)<<scr<<endl;
+    rcal=(avgr+scr)/2.0;
+   if(rcal>=5.0)
+   {
+     cout<<"Aluno aprovado."<<endl;
+   }else if(rcal<=4.9)
+   {
+    cout<<"Aluno reprovado."<<endl;
+   }
+   cout<<"Media final: "<< setprecision(1)<<rcal<<endl; 
+}
 }
